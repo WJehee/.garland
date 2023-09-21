fd978a346c5870ea96cdecafe4b012999f0bb655
Wouter Jehee <wouter@wouterjehee.com>
2023-09-21 18:25:39 +0200

    add openssh configuration to server

--- hosts/rusty-server/configuration.nix
+++ hosts/rusty-server/configuration.nix
@@ -16,6 +16,31 @@
         enable = true;
         allowedTCPPorts = [ 22 80 443 5232 ];
     };
+
+    services.openssh = {
+        enable = true;
+        settings.PasswordAuthentication = false;
+        settings.KbdInteractiveAuthentication = false;
+        settings.PermitRootLogin = "no";
+    };
+    users.users.admin = {
+        isNormalUser = true;
+        extraGroups = [
+            "wheel"
+            "docker"
+        ];
+        openssh.authorizedKeys.keys = [
+           "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCgwW3CKG28vJIpaQgdMr8y/Q6drSmo6id+A1KRwzhhfT8c3J7+S62zZHkNVbRFyn2PqVy6eBnZPP8I1vBreb4LwouF22NwDiuhvpju9cu6b2hjqUSuDaHwcNpB/HIqJfTKaHrbzVUHJRC/dXjs4azRZgIzG/d8RbAVkf8u1vT+MTIX3dGKLFhdw+ySK9uSlwfYK3XRst99GAA8aGz5rW9L0RmwaNgmIyMloM8fHUUZTM5pD5VouN9Gaf6lG2rbGAIxhxiUBM69X2sgejMMwNVlaGjIukNuffw6S4l64U4IyjEdzz51aC9Gya3MDuin+rgz+9cF+9ofcISttb6RWt6MqGfjlBlEhPFp5V56DD38zG/WFpnXkMM7ENXv51efFguPaVgYHJ83gmsKQ+n+yNOeeUjGF2f0h3mnOLTSPyo1BrorrPmWO59YWP3N+0EhFRoH3+/H7CVHm92oK0T0PLlJpaNVzxJSdzIPpfAIsTyT72MYfScgyUP1KS2gYzRibHk= wouter@rusty-desktop" 
+        ];
+    };
+    security.sudo.enable = false;
+    security.doas.enable = true;
+    security.doas.extraRules = [{
+        users = [ "admin" ];
+        keepEnv = true;
+        persist = true;
+    }];
+
     security.acme = {
         acceptTerms = true;
         defaults.email = "dev.temporator@aleeas.com";
--- modules/default.nix
+++ modules/default.nix
@@ -5,7 +5,6 @@
         ./packages.nix
         ./printing.nix
     ];
-    # General settings
     system.stateVersion = "23.05";
     nixpkgs.config.allowUnfree = true;
     time.timeZone = "Europe/Amsterdam";
@@ -14,7 +13,6 @@
     nix.gc.automatic = true;
     nix.gc.dates = "weekly";
     nix.gc.options = "--delete-older-than 30d";
-    # Services
     services.syncthing = {
         enable = true;
         user = "wouter";
@@ -23,23 +21,20 @@
     networking.networkmanager.enable = true;
     services.printing.enable = true;
     services.openssh.enable = true;
-    # Security
     security.pam.services.swaylock = {};
-    security.doas.enable = true;
     security.sudo.enable = true;
+    security.doas.enable = true;
     security.doas.extraRules = [{
         users = [ "wouter" ];
         keepEnv = true;
         persist = true;
     }];
-    # Zsh
     programs.zsh.enable = true;
     programs.zsh.setOptions = [
         "AUTO_CD"
         "COMPLETE_ALIASES"
     ];
     users.defaultUserShell = pkgs.zsh;
-    # Users
     users.users.wouter = {
         isNormalUser = true;
         extraGroups = [
