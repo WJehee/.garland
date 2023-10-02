7b5349edbe1123428a35fd913a9c5a42c168d8e5
Wouter Jehee <wouter@wouterjehee.com>
2023-10-02 15:54:59 +0200

    break nginx into module

--- hosts/rusty-server/configuration.nix
+++ hosts/rusty-server/configuration.nix
@@ -2,6 +2,7 @@
     imports = [
         ../../modules/boot.nix
         ../../modules/radicale.nix
+        ../../modules/nginx.nix
     ];
     environment.systemPackages = with pkgs; [
         git
@@ -42,34 +43,4 @@
         keepEnv = true;
         persist = true;
     }];
-
-    security.acme = {
-        acceptTerms = true;
-        defaults.email = "dev.temporator@aleeas.com";
-    };
-    services.nginx = {
-        enable = true;
-        recommendedProxySettings = true;
-        recommendedTlsSettings = true;
-        virtualHosts = {
-            "wouterjehee.com" = {
-                forceSSL = true;
-                enableACME = true;
-                root = "/home/admin/wouterjehee.com";
-                serverAliases = [ "cal.wouterjehee.com" ];
-            };
-           "cal.wouterjehee.com" = {
-               forceSSL = true;
-               useACMEHost = "wouterjehee.com";
-               locations."/" = {
-                   proxyPass = "http://localhost:5232/";
-                   extraConfig = ''
-                       proxy_set_header X-Script-Name /;
-                       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                       proxy_pass_header Authorization;
-                   '';
-               };
-           };
-        };
-    };
 }
