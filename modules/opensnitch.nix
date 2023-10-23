f6eb1c668fd5d0c56fac3cdd7feb8bb9d5858aa2
Wouter Jehee <wouter@wouterjehee.com>
2023-10-23 17:22:36 +0200

    add some sensible default rules for opensnitch, closes #12

--- modules/opensnitch.nix
+++ modules/opensnitch.nix
@@ -1,11 +1,73 @@
-{ ... }: {
+{ lib, pkgs, ... }: {
     services.opensnitch = {
-        enable = false;
+        enable = true;
         settings = {
             DefaultAction = "deny";
             DefaultDuration = "until restart";
             Firewall = "nftables";
             ProcMonitorMethod = "proc";
+        };
+        rules = {
+            systemd-timesyncd = {
+                name = "systemd-timesyncd";
+                enabled = true;
+                action = "allow";
+                duration = "always";
+                operator = {
+                    type ="simple";
+                    sensitive = false;
+                    operand = "process.path";
+                    data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-timesyncd";
+                };
+            };
+            systemd-resolved = {
+                name = "systemd-resolved";
+                enabled = true;
+                action = "allow";
+                duration = "always";
+                operator = {
+                    type ="simple";
+                    sensitive = false;
+                    operand = "process.path";
+                    data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-resolved";
+                };
+            };
+            spotify = {
+                name = "spotify";
+                enabled = true;
+                action = "allow";
+                duration = "always";
+                operator = {
+                    type = "simple";
+                    sensititve = false;
+                    operand = "process.path";
+                    data = "${lib.getBin pkgs.spotify}/share/spotify/.spotify-wrapped";
+                };
+            };
+            syncthing = {
+                name = "syncthing";
+                enabled = true;
+                action = "allow";
+                duration = "always";
+                operator = {
+                    type = "simple";
+                    sensitive = false;
+                    operand = "process.path";
+                    data = "${lib.getBin pkgs.syncthing}/bin/syncthing";
+                };
+            };
+            networkmanager = {
+                name = "networkmanager";
+                enable = true;
+                action = "allow";
+                duration = "always";
+                operator = {
+                    type = "simple";
+                    sensitive = false;
+                    operand = "process.path";
+                    data = "${lib.getBin pkgs.networkmanager}/bin/NetworkManager";
+                };
+            };
         };
     };
 }
