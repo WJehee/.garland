5ecadd95bd389a7d841a2e8ab7fbf13cbc0adcf2
Wouter Jehee <wouter@wouterjehee.com>
2023-11-21 19:43:44 +0100

    add cachix for hyprland flake, fix tab issue with nvim

--- modules/default.nix
+++ modules/default.nix
@@ -84,6 +84,10 @@
         alsa.support32Bit = true;
         pulse.enable = true;
     };
+    nix.settings = {
+        substituters = ["https://hyprland.cachix.org"];
+        trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
     programs.hyprland.enable = true;
     programs.dconf.enable = true;
     virtualisation.libvirtd.enable = true;
