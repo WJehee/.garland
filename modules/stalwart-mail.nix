28a0c16f1fc03aa8c93553ca67f02280e016d65c
Wouter Jehee <wouter@wouterjehee.com>
2023-12-01 17:58:21 +0100

    update + undo stalwart changes

--- modules/stalwart-mail.nix
+++ modules/stalwart-mail.nix
@@ -1,15 +1,6 @@
 { ... }: {
     services.stalwart-mail = {
         enable = true;
-	settings = {
-	    include.files = [
-		"/opt/stalwart-mail/etc/config.toml"
-	    ];
-	    global.tracing = {};
-	    queue = {};
-	    report = {};
-	    resolver = {};
-	    store = {};
-        };
+	    settings = {};
     };
 }
