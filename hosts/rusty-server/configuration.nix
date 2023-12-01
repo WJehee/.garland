34847ce0ad53dab4002232953441ff1e7afa97cc
Wouter Jehee <wouter@wouterjehee.com>
2023-12-01 19:12:30 +0100

    minimal working stalwart-mail configuration

--- hosts/rusty-server/configuration.nix
+++ hosts/rusty-server/configuration.nix
@@ -2,7 +2,7 @@
     imports = [
         ../../modules/radicale.nix
         ../../modules/nginx.nix
-		# ../../modules/stalwart-mail.nix
+		../../modules/stalwart-mail.nix
     ];
     boot.loader.grub = {
         enable = true;
@@ -50,6 +50,15 @@
                "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCgwW3CKG28vJIpaQgdMr8y/Q6drSmo6id+A1KRwzhhfT8c3J7+S62zZHkNVbRFyn2PqVy6eBnZPP8I1vBreb4LwouF22NwDiuhvpju9cu6b2hjqUSuDaHwcNpB/HIqJfTKaHrbzVUHJRC/dXjs4azRZgIzG/d8RbAVkf8u1vT+MTIX3dGKLFhdw+ySK9uSlwfYK3XRst99GAA8aGz5rW9L0RmwaNgmIyMloM8fHUUZTM5pD5VouN9Gaf6lG2rbGAIxhxiUBM69X2sgejMMwNVlaGjIukNuffw6S4l64U4IyjEdzz51aC9Gya3MDuin+rgz+9cF+9ofcISttb6RWt6MqGfjlBlEhPFp5V56DD38zG/WFpnXkMM7ENXv51efFguPaVgYHJ83gmsKQ+n+yNOeeUjGF2f0h3mnOLTSPyo1BrorrPmWO59YWP3N+0EhFRoH3+/H7CVHm92oK0T0PLlJpaNVzxJSdzIPpfAIsTyT72MYfScgyUP1KS2gYzRibHk= wouter@rusty-desktop" 
             ];
         };
+        users.stalwart-mail = {
+            isSystemUser = true;
+            home = "/var/lib/stalwart-mail";
+            group = "stalwart-mail";
+            extraGroups = [
+                "nginx"
+            ];
+        };
+        groups.stalwart-mail = {};
     };
     security = {
         sudo.enable = false;
--- modules/stalwart-mail.nix
+++ modules/stalwart-mail.nix
@@ -1,6 +1,47 @@
 { ... }: {
     services.stalwart-mail = {
         enable = true;
-	    settings = {};
+	    settings = {
+            macros = {
+                base_path = "/var/lib/stalwart-mail";
+                HOST = "mail.wouterjehee.com";
+                DEFAULT_DOMAIN = "wouterjehee.com";
+            };
+            include.files = [
+                "%{base_path}%/etc/common/tls.toml"
+                # "%{base_path}%/etc/common/tracing.toml"
+                "%{base_path}%/etc/common/sieve.toml"
+                "%{base_path}%/etc/directory/sql.toml"
+                "%{base_path}%/etc/imap/listener.toml"
+                "%{base_path}%/etc/imap/settings.toml"
+                "%{base_path}%/etc/jmap/auth.toml"
+                "%{base_path}%/etc/jmap/listener.toml"
+                "%{base_path}%/etc/jmap/oauth.toml"
+                "%{base_path}%/etc/jmap/protocol.toml"
+                "%{base_path}%/etc/jmap/push.toml"
+                "%{base_path}%/etc/jmap/ratelimit.toml"
+                # "%{base_path}%/etc/jmap/store.toml"
+                "%{base_path}%/etc/jmap/websockets.toml"
+                "%{base_path}%/etc/smtp/auth.toml"
+                "%{base_path}%/etc/smtp/listener.toml"
+                "%{base_path}%/etc/smtp/milter.toml"
+                # "%{base_path}%/etc/smtp/queue.toml"
+                "%{base_path}%/etc/smtp/remote.toml"
+                # "%{base_path}%/etc/smtp/report.toml"
+                # "%{base_path}%/etc/smtp/resolver.toml"
+                "%{base_path}%/etc/smtp/session.toml"
+                "%{base_path}%/etc/smtp/signature.toml"
+                "%{base_path}%/etc/smtp/spamfilter.toml" 
+            ];
+            server = {
+                hostname = "%{HOST}%";
+                max-connections = 8192;
+                socket = {
+                    nodelay = true;
+                    reuse-addr = true;
+                    backlog = 1024;
+                };
+            };
+        };
     };
 }
