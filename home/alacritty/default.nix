b749dd9510847fe95d0afb4f1cbe9d893d9c930f
Wouter Jehee <wouter@wouterjehee.com>
2023-12-05 23:01:37 +0100

    fix alacritty colorscheme?

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -25,12 +25,12 @@
                 };
                 bright = {
                     black = "#${config.colorScheme.colors.base03}";
-                    red = "#${config.colorScheme.colors.base09}";
-                    green = "#${config.colorScheme.colors.base01}";
-                    yellow = "#${config.colorScheme.colors.base02}";
-                    blue = "#${config.colorScheme.colors.base04}";
-                    magenta = "#${config.colorScheme.colors.base06}";
-                    cyan = "#${config.colorScheme.colors.base0F}";
+                    red = "#${config.colorScheme.colors.base08}";
+                    green = "#${config.colorScheme.colors.base0B}";
+                    yellow = "#${config.colorScheme.colors.base0A}";
+                    blue = "#${config.colorScheme.colors.base0D}";
+                    magenta = "#${config.colorScheme.colors.base0E}";
+                    cyan = "#${config.colorScheme.colors.base0C}";
                     white = "#${config.colorScheme.colors.base07}";
                 };
             };
