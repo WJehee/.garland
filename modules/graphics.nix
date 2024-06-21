cddbec68974486b657b8fe785302419d7e9d9b0c
Wouter Jehee <wouter@wouterjehee.com>
2024-06-21 16:45:32 +0200

    update packages, remove deprecated option

--- modules/graphics.nix
+++ modules/graphics.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }: {
     hardware.opengl = {
         enable = true;
-        driSupport = true;
         driSupport32Bit = true;
         extraPackages = with pkgs; [
             mesa.drivers
