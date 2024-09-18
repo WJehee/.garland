0facb75c1c638523f16d2d991f7360556fa42569
Wouter Jehee <wouter@wouterjehee.com>
2024-09-18 16:45:14 +0200

    add nushell stuff

--- home/nushell.nix
+++ home/nushell.nix
@@ -1,30 +1,36 @@
 { ... }: {
-    programs.nushell = {
-        enable = true;
-        configFile.text = ''
-            $env.config = {
-                show_banner: false,
-            }
+    programs = {
+        nushell = {
+            enable = true;
+            configFile.text = ''
+                $env.config = {
+                    show_banner: false,
+                }
 
-        '';
-        envFile.text = ''
-            $env.STARSHIP_SHELL = "nu"
+            '';
+            envFile.text = ''
+                $env.STARSHIP_SHELL = "nu"
 
-            def create_left_prompt [] {
-                starship prompt --cmd-duration $env.CMD_DURATION_MS $'--status=($env.LAST_EXIT_CODE)'
-            }
+                def create_left_prompt [] {
+                    starship prompt --cmd-duration $env.CMD_DURATION_MS $'--status=($env.LAST_EXIT_CODE)'
+                }
 
-            $env.PROMPT_COMMAND = { || create_left_prompt }
-            $env.PROMPT_COMMAND_RIGHT = ""
+                $env.PROMPT_COMMAND = { || create_left_prompt }
+                $env.PROMPT_COMMAND_RIGHT = ""
 
-            $env.PROMPT_INDICATOR = ""
-            $env.PROMPT_INDICATOR_VI_INSERT = ": "
-            $env.PROMPT_INDICATOR_VI_NORMAL = "〉"
-            $env.PROMPT_MULTILINE_INDICATOR = "::: "
-        '';
-        shellAliases = {
-            la = "ls -a";
-            nd = "nix develop -c nu";
+                $env.PROMPT_INDICATOR = ""
+                $env.PROMPT_INDICATOR_VI_INSERT = ": "
+                $env.PROMPT_INDICATOR_VI_NORMAL = "〉"
+                $env.PROMPT_MULTILINE_INDICATOR = "::: "
+            '';
+            shellAliases = {
+                la = "ls -a";
+                nd = "nix develop -c nu";
+            };
+        };
+        carapace = {
+            enable = true;
+            enableNushellIntegration = true;
         };
     };
-         }
+}
