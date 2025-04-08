831ae5ae2c722862526dac73e8778819ebdb7e55
Wouter Jehee <wouter@wouterjehee.com>
2025-04-08 21:24:12 +0200

    package updates + switch DNS

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,7 +1,8 @@
 { lib, ... }: {
     networking = {
         networkmanager.enable = true;
-        nameservers = [ "9.9.9.9" ];
+        # nameservers = [ "9.9.9.9" ];      # Quad9
+        nameservers = [ "194.242.2.6" ];     # Mullvad
     };
     systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
     systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
