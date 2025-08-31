78e1941a54332ebb6d832d1aaf84834934139206
Wouter Jehee <wouter@wouterjehee.com>
2025-08-31 13:02:15 +0200

    change monitor config foxglove

--- nixos/foxglove/home.nix
+++ nixos/foxglove/home.nix
@@ -4,20 +4,23 @@
     ];
     wayland.windowManager.hyprland.settings = {
         monitor = [
+            ", preferred, auto, 1"
             "eDP-1, preferred, auto, 1.2"
+            "DP-5, preferred, auto-left, 1, transform, 1"
+            "DP-6, preferred, auto-left, 1"
         ];
         workspace = [
-            "1, monitor:eDP-1"
-            "2, monitor:eDP-1"
-            "3, monitor:eDP-1"
-            "4, monitor:eDP-1"
-            "5, monitor:eDP-1"
+            "1, monitor:DP-6"
+            "2, monitor:DP-6"
+            "3, monitor:DP-6"
+            "4, monitor:DP-6"
+            "5, monitor:DP-6"
 
-            "6, monitor:DP-1"
-            "7, monitor:DP-1"
-            "8, monitor:DP-1"
-            "9, monitor:DP-1"
-            "10, monitor:DP-1"
+            "6, monitor:DP-5"
+            "7, monitor:DP-5"
+            "8, monitor:DP-5"
+            "9, monitor:DP-5"
+            "10, monitor:eDP-1"
         ];
     };
     xdg.configFile."hypr/hyprpaper.conf".text = ''
