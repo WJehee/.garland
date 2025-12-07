b8eb95b7cc98fc4155eca1d9de0f6791bef5ec25
Wouter Jehee <wouter@wouterjehee.com>
2025-12-07 21:04:23 +0100

    update packages and remove dorusrijkers.club

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -10,14 +10,6 @@
             "cal.wouterjehee.com".extraConfig = ''
                 reverse_proxy http://localhost:5232
             '';
-            "dorusrijkers.club".extraConfig = ''
-                root * /var/www/dorusrijkers.eu
-                encode gzip
-                file_server
-            '';
-            "loodsenboekje.dorusrijkers.club".extraConfig = ''
-                reverse_proxy http://localhost:1744
-            '';
             "dorusrijkers.eu".extraConfig = ''
                 root * /var/www/dorusrijkers.eu
                 encode gzip
