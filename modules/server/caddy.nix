884d9989a61d07c35f88464d0209457b0fb21af6
Wouter Jehee <wouter@wouterjehee.com>
2026-02-13 14:58:52 +0100

    add authentik snippet to caddy for providing auth

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -1,6 +1,17 @@
 { ... }: {
     services.caddy = {
         enable = true;
+        globalConfig = ''
+            (authentik) {
+                reverse_proxy /outpost.goauthentik.io/* localhost:9000
+
+                forward_auth localhost:9000 {
+                    uri /outpost.goauthentik.io/auth/caddy
+                    copy_headers X-Authentik-Username X-Authentik-Groups X-Authentik-Entitlements X-Authentik-Email X-Authentik-Name X-Authentik-Uid X-Authentik-Jwt X-Authentik-Meta-Jwks X-Authentik-Meta-Outpost X-Authentik-Meta-Provider X-Authentik-Meta-App X-Authentik-Meta-Version
+                    trusted_proxies private_ranges
+                }
+            }
+        '';
         virtualHosts = {
             "wouterjehee.com".extraConfig = ''
                 root * /var/www/wouterjehee.com
@@ -13,11 +24,16 @@
             "img.wouterjehee.com".extraConfig = ''
                 reverse_proxy http://localhost:2283
             '';
-
             "auth.wouterjehee.com".extraConfig = ''
                 reverse_proxy http://localhost:9000
             '';
-
+            "app1.wouterjehee.com".extraConfig = ''
+                route {
+                    import authentik
+                    reverse_proxy localhost:4000
+                }
+            '';
+            
             "dorusrijkers.eu".extraConfig = ''
                 root * /var/www/dorusrijkers.eu
                 encode gzip
