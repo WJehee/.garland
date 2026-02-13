e46fe7549e3615a5c33174f3591d76e33964347c
Wouter Jehee <wouter@wouterjehee.com>
2026-02-13 17:39:56 +0100

    small fixes, add custom module for my own software

---
+++ modules/server/custom.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }: {
+    # Website deploy user
+    users.users.decree = {
+        isSystemUser = true;
+        group = "decree";
+        shell = "${pkgs.coreutils}/bin/false";
+        openssh.authorizedKeys.keys = [
+            # Key defind in Github secrets
+            ''command="${pkgs.rrsync}/bin/rrsync /var/www/wouterjehee.com",restrict ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII9Ak2oGjRlLkDPHwm8u59i3NkyBIQ/6r9KpkDt1jbbz wouter@foxglove''
+        ];
+    };
+    users.groups.decree = {};
+    systemd.tmpfiles.rules = [
+        "d /var/www/wouterjehee.com 0755 decree decree -"
+    ];
+    services.loodsenboekje.enable = true;
+}
