1eeb064afdbf38839df40080ee8d933b00833893
Wouter Jehee <wouter@wouterjehee.com>
2026-02-21 14:08:22 +0100

    package updates + update claude memory

--- home/dev/llm.nix
+++ home/dev/llm.nix
@@ -7,13 +7,20 @@
         enable = true;
         skills = {};
         memory.text = ''
-            # Version control
+            # Projects
+            All my machines run NixOS.
+            All projects should include a nix flake for installing, running and building the project.
+
+            All projects should have a simple justfile for running common commands for using in the project.
+            Basic flakes to get started can be found at https://github.com/wjehee/.garland in the template sfolder.
+            The link also contains the nix flake that exposes templates for most languages I use.
+
             I use jj (Jujutsu) as my version control system, backed by git.
             Always use jj commands instead of git commands whenever possible.
             For example, use `jj status` instead of `git status`, `jj log` instead of `git log`, etc.
             
             # Writing
-            When writing, please do not use em dashes.
+            When writing, please do not use em dashes and emoji's 🙏.
         '';
     };
 }
