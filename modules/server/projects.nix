00d7866363321c0acd86732b6e5135765e1db74a
Wouter Jehee <wouter@wouterjehee.com>
2026-08-02 19:25:18 +0200

    Fix proxy

--- modules/server/projects.nix
+++ modules/server/projects.nix
@@ -30,7 +30,7 @@
             "6767ov.dorusrijkers.eu".extraConfig = ''
                 route {
                     import authelia
-                    reverse_proxy http://foxglove:6767
+                    reverse_proxy http://dr-perron:6767
                 }
             '';
         };
